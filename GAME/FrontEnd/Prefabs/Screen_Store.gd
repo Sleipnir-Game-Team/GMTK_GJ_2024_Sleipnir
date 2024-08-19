5fa1dbe87196b32e95df260027581634ce04cc5e
IanMateus <ianmateus.souza@hotmail.com>
2024-08-18 22:31:55 -0400

    Atualização de UI
    Agora o menu inicial tá quase completo e consegue dar stretch sem sair desmontando a tela inicial, o tamanho padrão da tela também mudou e agora a tela acompanha o tamanho da tela que está abrindo o jogo, a tela da loja de dentro do jogo também começou a ser construída

---
+++ GAME/FrontEnd/Prefabs/Screen_Store.gd
@@ -0,0 +1,25 @@
+extends Control
+
+signal buyItem
+
+func _on_button_buy_item_1_pressed():
+	buyItem.emit("1")
+
+func _on_button_buy_item_2_pressed():
+	buyItem.emit("2")
+
+
+func _on_button_buy_item_3_pressed():
+	buyItem.emit("3")
+
+
+func _on_button_buy_item_4_pressed():
+	buyItem.emit("4")
+
+
+func _on_button_buy_item_5_pressed():
+	buyItem.emit("5")
+
+
+func _on_button_buy_item_6_pressed():
+	buyItem.emit("6")
