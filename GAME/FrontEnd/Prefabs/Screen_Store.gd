4e0e5e819da5c4121a01c27fcefde8721468299d
IanMateus <ianmateus.souza@hotmail.com>
2024-08-19 02:25:47 -0400

    Menu inicial finalizado (creio eu)
    Agora o menu inicial tá finalmente pronto (creio eu), e o menu da loja tá começando a ficar pog

--- GAME/FrontEnd/Prefabs/Screen_Store.gd
+++ GAME/FrontEnd/Prefabs/Screen_Store.gd
@@ -2,8 +2,12 @@ extends Control
 
 signal buyItem
 
+func _ready():
+	currencyEditor("100")
+
 func _on_button_buy_item_1_pressed():
 	buyItem.emit("1")
+
 
 func _on_button_buy_item_2_pressed():
 	buyItem.emit("2")
@@ -23,3 +27,7 @@ func _on_button_buy_item_5_pressed():
 
 func _on_button_buy_item_6_pressed():
 	buyItem.emit("6")
+
+
+func currencyEditor(points):
+	$CanvasLayer/Player_Points/Player_Points.text = points
--- GAME/FrontEnd/Scripts/Screen_MainMenu.gd
+++ GAME/FrontEnd/Scripts/Screen_MainMenu.gd
@@ -4,6 +4,7 @@ func _ready():
 	UI_Displayer.screens.append(self)
 	get_viewport().size = DisplayServer.screen_get_size()
 	#get_viewport().
+	$CanvasLayer/AnimationPlayer.play("Flickering")
 
 func _on_button_play_pressed():
 	UI_Controller.manageScreen("res://Levels/Dungeon.tscn", get_tree().root, "change")
@@ -15,3 +16,27 @@ func _on_button_options_pressed():
 
 func _on_button_quit_pressed():
 	get_tree().quit()
+
+
+func _on_button_options_mouse_entered():
+	get_tree().get_nodes_in_group("Ilumination")[1].modulate.a = 1
+
+
+func _on_button_options_mouse_exited():
+	get_tree().get_nodes_in_group("Ilumination")[1].modulate.a = 0.5
+
+
+func _on_button_play_mouse_entered():
+	get_tree().get_nodes_in_group("Ilumination")[0].modulate.a = 1
+
+
+func _on_button_play_mouse_exited():
+	get_tree().get_nodes_in_group("Ilumination")[0].modulate.a = 0.5
+
+
+func _on_button_quit_mouse_entered():
+	get_tree().get_nodes_in_group("Ilumination")[2].modulate.a = 1
+
+
+func _on_button_quit_mouse_exited():
+	get_tree().get_nodes_in_group("Ilumination")[2].modulate.a = 0.5
