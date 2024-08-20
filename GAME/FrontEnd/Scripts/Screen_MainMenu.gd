9184b9a0719551032325e86af045edf9c1ab41d5
IanMateus <ianmateus.souza@hotmail.com>
2024-08-20 00:53:18 -0400

    Mb tava crashando o jogo
    Não comentei duas linhas de código e tava crashando o jogo por tentar abrir músicas que não existem

--- GAME/FrontEnd/Scripts/Screen_MainMenu.gd
+++ GAME/FrontEnd/Scripts/Screen_MainMenu.gd
@@ -5,8 +5,8 @@ func _ready():
 	get_viewport().size = DisplayServer.screen_get_size()
 	#get_viewport().
 	$CanvasLayer/AnimationPlayer.play("Flickering")
-	SleipnirMaestro.change_song("menu")
-	SleipnirMaestro.play()
+	#SleipnirMaestro.change_song("menu")
+	#SleipnirMaestro.play()
 
 func _on_button_play_pressed():
 	UI_Controller.manageScreen("res://FrontEnd/Prefabs/Screen_Cutscene.tscn", get_tree().root, "change")
